f6a5e0f7b8c8ad7017fdbeb95693fbcd49b610fc
mastermuskan22 <Rita@22Praveen>
2025-06-16 23:45:47 +0530

    commit

--- 10structs/main.go
+++ 10structs/main.go
@@ -12,6 +12,6 @@ type User struct {
 func main() {
 	Muskan := User{"Muskan", 23, "muskan@gmail.com", true}
 	fmt.Println(Muskan)
-	fmt.Printf("The user is : %+v\n",Muskan)
-	fmt.Printf("The name is %v and age is %v",Muskan.Name,Muskan.Age)
+	fmt.Printf("The user is : %+v\n", Muskan)
+	fmt.Printf("The name is %v and age is %v", Muskan.Name, Muskan.Age)
 }
