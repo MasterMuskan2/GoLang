52228ef4c9b2e71e0d10776db3f675ed11ddb943
mastermuskan22 <Rita@22Praveen>
2025-10-16 20:40:03 +0530

    new changes

--- 24MongoDB/main.go
+++ 24MongoDB/main.go
@@ -1,1 +1,17 @@
 package main
+
+import (
+	"fmt"
+	"log"
+	"net/http"
+
+	"github.com/mastermuskan22/mongodbapi/router"
+)
+
+func main() {
+	fmt.Println("MY API")
+	r := router.Router()
+	fmt.Println("The server is getting started.......")
+	log.Fatal(http.ListenAndServe(":2000", r))
+	fmt.Println("Listening to the port 2000.......")
+}
